ae55364dc75032afadfb5f46049415538f1f0feb
Arex360 <49303647+Arex360@users.noreply.github.com>
2020-01-14 08:08:43 +0500

    Create Linear.cpp

---
+++ Linear.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+class Linear{
+  public:
+     bool present;
+     int ans;
+     int search(int arr[], int n, int x);
+};
+int Linear::search(int arr[], int n, int x){
+     for(int i=0;i<n;i++){
+       if(arr[i] == x){
+         present = true;
+         ans = i;
+         break;
+       }
+     }
+     if(present == 1){
+       return ans;
+     }
+     else if (present == 0){
+       return -9999;
+     }
+}
+int main(){
+  int n;
+  cout << "Enter the range: ";
+  cin >> n;
+  int a[n];
+  for(int i=0;i<n;i++){
+    cout << "Enter the number" << i+1 << ": ";
+    cin >> a[i];
+  }
+  cout << "Enter number you want to search: ";
+  int x;
+  cin >> x;
+  Linear L;
+  int b = L.search(a, n,x);
+  if(b== -9999){
+    cout << "Number doesn't exist";
+  }
+  else{
+    cout << "At index " << b;
+  }
+}
